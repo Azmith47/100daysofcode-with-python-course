98a1b7e7aef4401ea5e06804ed9b6fbaa967d2ff
Azmith47 <ev_tv47@hotmail.com>
2023-01-22 09:26:07 +1100

    solved Bite 22 decorators

--- days/22-24-decorators/decorators.py
+++ days/22-24-decorators/decorators.py
@@ -2,4 +2,14 @@ from functools import wraps
 
 
 def make_html(element):
-    pass
+    def inner(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+
+            response = func(*args, **kwargs)
+
+            return f"<{element}>{response}</{element}>"
+
+        return wrapper
+
+    return inner
--- days/22-24-decorators/test_decorators.py
+++ days/22-24-decorators/test_decorators.py
@@ -5,6 +5,7 @@ def test_make_html():
     @make_html("p")
     @make_html("strong")
     def get_text(text="I code with PyBites"):
+        """Returns text"""
         return text
 
     assert get_text() == "<p><strong>I code with PyBites</strong></p>"

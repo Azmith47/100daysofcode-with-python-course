376fa2ad29a707934eb779ba6465c38cb617d47c
Azmith47 <ev_tv47@hotmail.com>
2023-01-22 10:13:42 +1100

    Update login_decorator.py

--- days/22-24-decorators/login_decorator.py
+++ days/22-24-decorators/login_decorator.py
@@ -4,13 +4,28 @@ known_users = ["bob", "julian", "mike", "carmen", "sue"]
 loggedin_users = ["mike", "sue"]
 
 
-def login_required():
+def login_required(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        pass
+        for arg in args:
+            if arg not in known_users:
+                return "please create an account"
+            elif arg not in loggedin_users:
+                return "please login"
+            else:
+                return func(*args, **kwargs)
+
+    return wrapper
 
 
 @login_required
 def welcome(user):
     """Return a welcome message if logged in"""
     return f"welcome back {user}"
+
+
+print(welcome("bob"))
+
+print(welcome("mike"))
+
+print(welcome("tory"))

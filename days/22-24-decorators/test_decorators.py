30cc04c977b22d66813a07844a3ac5759b22352a
Azmith47 <ev_tv47@hotmail.com>
2023-01-22 16:18:01 +1100

    Update test_decorators.py

--- days/22-24-decorators/test_decorators.py
+++ days/22-24-decorators/test_decorators.py
@@ -1,11 +1,34 @@
 from decorators import make_html
+import pytest
 
 
-def test_make_html():
-    @make_html("p")
-    @make_html("strong")
+@pytest.mark.parametrize(
+    "arg, elem1, elem2, ret",
+    [
+        (
+            "Hello Christian",
+            "h1",
+            "strong",
+            "<h1><strong>Hello Christian</strong></h1>",
+        ),
+        ("how do you do?", "li", "p", "<li><p>how do you do?</p></li>"),
+        (
+            "whats happening Harry",
+            "p",
+            "strong",
+            "<p><strong>whats happening Harry</strong></p>",
+        ),
+        (None, "p", "strong", "<p><strong>I code with PyBites</strong></p>"),
+    ],
+)
+def test_make_html(arg, elem1, elem2, ret):
+    @make_html(elem1)
+    @make_html(elem2)
     def get_text(text="I code with PyBites"):
         """Returns text"""
         return text
 
-    assert get_text() == "<p><strong>I code with PyBites</strong></p>"
+    if arg != None:
+        assert get_text(arg) == ret
+    else:
+        assert get_text() == ret
